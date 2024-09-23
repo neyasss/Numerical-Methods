ce37d77fec6af4efb354ca378a454397dd363f3d
Irina Zackieva <125659282+neyasss@users.noreply.github.com>
2024-09-23 16:54:58 +0300

    Header.h

--- Header.h
+++ Header.h
@@ -11,29 +11,29 @@ using namespace std;
 
 void readSLAE(const string& file, vector<vector<T>>& A, vector<T>& b); // чтение СЛАУ из файла
 
-void printSLAE(const vector<vector<T>>& A, const vector<T>& b); // вывод СЛАУ на экран
-void printMatrix(const vector<vector<T>>& A); // вывод матрицы на экран
+void printSLAE(const vector<vector<T>>& A, const vector<T>& b, int n); // вывод СЛАУ на экран
+void printMatrix(const vector<vector<T>>& A, int n); // вывод матрицы на экран
 
-vector<vector<T>> MatrixMult(const vector<vector<T>>& A, const vector<vector<T>>& B); // умножение матриц
-vector<vector<T>> Transpose(const vector<vector<T>>& A); // транспонирование (для QR-разложения)
+vector<vector<T>> MatrixMult(const vector<vector<T>>& A, const vector<vector<T>>& B, int n); // умножение матриц
+vector<vector<T>> Transpose(const vector<vector<T>>& A, int n); // транспонирование (для QR-разложения)
 
-vector<T> GaussianMethod(vector<vector<T>>& A, vector<T>& b); // Метод Гаусса с частичным выбором главного элемента (по столбцу)
+vector<T> GaussianMethod(vector<vector<T>>& A, vector<T>& b, int n); // Метод Гаусса с частичным выбором главного элемента (по столбцу)
 
-vector<T> QRMethod(vector<vector<T>>& A, vector<T>& b); // Решение методом QR-разложения
+vector<T> QRMethod(vector<vector<T>>& A, vector<T>& b, int n); // Решение методом QR-разложения
 
-void ResidualVectorNorm(const vector<vector<T>>& A, const vector<T>& b, const vector<T>& x); // норма вектора невязки (обе нормы)
+void ResidualVectorNorm(const vector<vector<T>>& A, const vector<T>& b, const vector<T>& x, int n); // норма вектора невязки (обе нормы)
 
-vector<vector<T>> InvLU(const vector<vector<T>>& A); // нахождение обратной матрицы с помощью LU-разложения
+vector<vector<T>> InvLU(const vector<vector<T>>& A, int n); // нахождение обратной матрицы с помощью LU-разложения
 
-T vectorNorm1(const vector<T>& b); // векторная октаэдрическая норма
-T vectorNormInf(const vector<T>& b); // векторная кубическая норма
+T vectorNorm1(const vector<T>& b, int n); // векторная октаэдрическая норма
+T vectorNormInf(const vector<T>& b, int n); // векторная кубическая норма
 
-T matrixNorm1(const vector<vector<T>>& A); // матричная октаэдрическая норма
-T matrixNormInf(const vector<vector<T>>& A); // матричная кубическая норма
+T matrixNorm1(const vector<vector<T>>& A, int n); // матричная октаэдрическая норма
+T matrixNormInf(const vector<vector<T>>& A, int n); // матричная кубическая норма
 
 // Число обусловленности для различных матричных норм
-T cond1(const vector<vector<T>>& A);
-T condInf(const vector<vector<T>>& A);
+T cond1(const vector<vector<T>>& A, int n);
+T condInf(const vector<vector<T>>& A, int n);
 
-void condEstimation(vector<vector<T>>& A, vector<T>& b, const vector<T>& disturb); // вектор возмущения как аргумент, вычисляет 1 раз
-void condEstimationLower(vector<vector<T>>& A, vector<T>& b, int disturbCount = 5); // решение несколько раз и оценка
+void condEstimation(vector<vector<T>>& A, vector<T>& b, const vector<T>& disturb, int n); // вектор возмущения как аргумент, вычисляет 1 раз
+void condEstimationLower(vector<vector<T>>& A, vector<T>& b, int n, int disturbCount = 5); // решение несколько раз и оценка
