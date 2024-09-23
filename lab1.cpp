558834f82fe4260940ee30feb6308c455770cec7
Irina Zackieva <125659282+neyasss@users.noreply.github.com>
2024-09-23 16:57:26 +0300

    lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -14,38 +14,38 @@ int main()
     int n = A.size();
 
     cout << "Исходная СЛАУ:" << endl;
-    printSLAE(A, b);
+    printSLAE(A, b, n);
     cout << endl;
 
     cout << "Решение СЛАУ методом Гаусса:" << endl;
-    vector<T> solution1 = GaussianMethod(A, b);
+    vector<T> solution1 = GaussianMethod(A, b, n);
     for (int i = 0; i < n; i++)
         cout << "x" << i + 1 << " = " << solution1[i] << endl;
 
-    ResidualVectorNorm(A, b, solution1);
+    ResidualVectorNorm(A, b, solution1, n);
 
     cout << endl << "Решение СЛАУ методом QR-разложения:" << endl;
-    vector<T> solution2 = QRMethod(A, b);
+    vector<T> solution2 = QRMethod(A, b, n);
     for (int i = 0; i < n; i++)
         cout << "x" << i + 1 << " = " << solution2[i] << endl;
 
-    ResidualVectorNorm(A, b, solution2);
+    ResidualVectorNorm(A, b, solution2, n);
 
-    vector<vector<T>> Ainv = InvLU(A);
+    vector<vector<T>> Ainv = InvLU(A, n);
     cout << endl << "Обратная матрица для матрицы системы A:" << endl;
-    printMatrix(Ainv);
+    printMatrix(Ainv, n);
 
     cout << endl << "Число обусловленности матрицы A при использовании" << endl;
-    cout << "октаэдрической нормы: " << cond1(A) << endl;
-    cout << "кубической нормы: " << condInf(A) << endl;
+    cout << "октаэдрической нормы: " << cond1(A, n) << endl;
+    cout << "кубической нормы: " << condInf(A, n) << endl;
 
     vector<T> disturb = { 0.01, 0.01, 0.01, 0.01 }; // возмущение
-    // condEstimation(A, b, disturb);
+    // condEstimation(A, b, disturb, n);
 
     cout << endl << "Оценка числа обусловленности:";
-    condEstimationLower(A, b);
+    condEstimationLower(A, b, n);
 
     cout << endl << "Результат умножения A^(-1) на A:" << endl;
-    vector<vector<T>> M = MatrixMult(A, Ainv);
-    printMatrix(M);
+    vector<vector<T>> M = MatrixMult(A, Ainv, n);
+    printMatrix(M, n);
 }
