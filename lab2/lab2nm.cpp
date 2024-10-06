f81993856372323794d196350d9cbec97a65b521
Irina Zackieva <125659282+neyasss@users.noreply.github.com>
2024-10-06 21:04:00 +0300

    Update lab2nm.cpp

--- lab2/lab2nm.cpp
+++ lab2/lab2nm.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 #include <vector>
 #include <fstream>
@@ -18,15 +18,33 @@ int main()
     cout << endl;
 
     vector<T> x0 = b; // в качестве начального приближения берем вектор правой части
+    const T eps = 1e-4;
+    const T tau = 0.015;
+    const T omega = 1.2;
+    const int norm = 1; // 1 - октаэдрическая, 0 - кубическая (inf)
 
-    Params resultSimpleIter = SimpleIterationMethod(A, b, x0, 0.015, 1e-4, n, 1);
+    cout << "МЕТОД ПРОСТОЙ ИТЕРАЦИИ:" << endl;
+    Params resultSimpleIter = SimpleIterationMethod(A, b, x0, tau, eps, n, norm);
     for (int i = 0; i < n; i++)
         cout << "x" << i + 1 << " = " << resultSimpleIter.x[i] << endl;
     cout << "Количество итераций: " << resultSimpleIter.iterCount << endl;
     // cout << resultSimpleIter.normC1 << endl;
 
-    Params resultJacobi = JacobiMethod(A, b, x0, 1e-4, n, 1);
+    cout << endl << "МЕТОД ЯКОБИ:" << endl;
+    Params resultJacobi = JacobiMethod(A, b, x0, eps, n, norm);
     for (int i = 0; i < n; i++)
         cout << "x" << i + 1 << " = " << resultJacobi.x[i] << endl;
     cout << "Количество итераций: " << resultJacobi.iterCount << endl;
+
+    cout << endl << "МЕТОД ЗЕЙДЕЛЯ:" << endl;
+    Params resultSeidel = SeidelMethod(A, b, x0, eps, n, norm);
+    for (int i = 0; i < n; i++)
+        cout << "x" << i + 1 << " = " << resultSeidel.x[i] << endl;
+    cout << "Количество итераций: " << resultSeidel.iterCount << endl;
+
+    cout << endl << "МЕТОД РЕЛАКСАЦИИ:" << endl;
+    Params resultRelaxation = RelaxationMethod(A, b, x0, omega, eps, n, norm);
+    for (int i = 0; i < n; i++)
+        cout << "x" << i + 1 << " = " << resultRelaxation.x[i] << endl;
+    cout << "Количество итераций: " << resultRelaxation.iterCount << endl;
 }
