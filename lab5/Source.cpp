ed046f694947fb2559777f773f79aadb366ca01c
Emilia Burak <125658991+eemides@users.noreply.github.com>
2024-12-17 00:41:18 +0300

    Update Source.cpp

--- lab5/Source.cpp
+++ lab5/Source.cpp
@@ -49,6 +49,17 @@ vector<numtype> operator-(const vector<numtype>& vec1, const vector<numtype>& ve
 	return vec3;
 }
 
+ostream& operator<<(ostream& out, const vector<numtype>& vec) {
+	if (vec.size() > 0) {
+		out << vec[0];
+		for (size_t i = 1; i < vec.size(); ++i)
+			out << " " << vec[i];
+	}
+	else out << "null";
+	return out;
+}
+
+
 vector<double> localize(const function<double(double)>&f, double left, double right, size_t div) {
 		cout << "Локализация корней:" << endl << "новый промежуток: ";
 		list<double> sections;  //список начал и концов отрезка
@@ -71,7 +82,7 @@ vector<double> localize(const function<double(double)>&f, double left, double ri
 			cout << *(sec++) << " -- ";
 			cout << *(sec++) << "\n";
 		}
-		cout << std::endl;
+		cout << endl;
 		vector<double> result(sections.size());
 		copy(sections.begin(), sections.end(), result.begin());
 		return result;
@@ -146,7 +157,7 @@ double newton(const function<double(double)>& f, double a, double b, double eps,
 		};
 	else protectRange = [](double) {};
 
-	std::function<void()> protectLoop; //защита от зацикливания
+	function<void()> protectLoop; //защита от зацикливания
 	if (loopProtection)
 		protectLoop = [&]() {
 		for (auto it = xList.begin(); it != xList.end(); ++it)
@@ -180,8 +191,9 @@ double newton(const function<double(double)>& f, double a, double b, double eps,
 	return xNext;
 }
 
-vector<double> newton_2(function<double(double, double)>& func1, function<double(double, double)>& func2, double eps, double startX, double startY) {
-	/*vector<double> xNext({startX, startY}), xPrev(2);
+vector<double> newton_2(const function<double(double, double)>& func1,const function<double(double, double)>& func2, double eps, double startX, double startY) {
+	cout << "Метод Ньютона для системы из 2-х уравнений: " << endl << endl;
+	vector<double> xNext({startX, startY}), xPrev(2);
 	const double h = 1e-6;
 	double F11, F12, F21, F22;
 	do {
@@ -204,72 +216,119 @@ vector<double> newton_2(function<double(double, double)>& func1, function<double
 		xNext[0] = xPrev[0] - (F22 * func1(xPrev[0], xPrev[1]) - F12 * func2(xPrev[0], xPrev[1])) / detF;
 		xNext[1] = xPrev[1] - (-F21 * func1(xPrev[0], xPrev[1]) + F11 * func2(xPrev[0], xPrev[1])) / detF;
 	} while (normInf(xNext - xPrev) > eps);
+	cout << "Решения системы в заданной области: " << xNext << endl;
 	return xNext;
-	*/
+	
+}
+
+vector<double> newtonDiagram(const function<double(double, double)>& func1, const function<double(double, double)>& func2, size_t N, const string& fileName) {
+	cout <<endl << "Диаграмма сходимости: " << endl;
+	const double eps = 1e-6;
+
+	bool analytical = false;
+	//analytical = true;
+
+	vector<double> xGrid, yGrid;
+	double xR = 10., yR = 10.;
+	double hx = 2 * xR / (double)N, hy = 2 * yR / (double)N;
+
+	vector<vector<unsigned>> square;
+	square.resize(N);
+	for (size_t i = 0; i < N; ++i)
+		square[i].resize(N);
+
+	vector<double> xNext(2, 0), xPrev(2, 0);
+
+	unsigned iter = 0;
+	const double h = 1e-6;
 
 	double F11, F12, F21, F22;
-	vector<double> xtmp(2, 0), xcur(2, 0), xprev(2, 0);
-	vector<double> a(2, 0), b(2, 0);
-	xcur = { startX, startY };
-	xtmp = { 0, 0 };
-	int iter = 0;
-
-	cout << "Метод Ньютона для системы: " << endl << endl;
-	while (abs(sqr(xcur, xtmp)) > eps / 10) {
-
-		cout << "(" << xcur[0] << ", " << xcur[1] << ")" << endl;
-
-		iter++;
-		if (iter > 30) {
-			iter = 30;
-			break;
-		}
-		xtmp = xcur;
-
-		F11 = (func1(xtmp[0] + eps, xtmp[1]) - func1(xtmp[0] - eps, xtmp[1])) / (2 * eps);
-		F12 = (func1(xtmp[0], xtmp[1] + eps) - func1(xtmp[0], xtmp[1] - eps)) / (2 * eps);
-		F21 = (func2(xtmp[0] + eps, xtmp[1]) - func2(xtmp[0] - eps, xtmp[1])) / (2 * eps);
-		F22 = (func2(xtmp[0], xtmp[1] + eps) - func2(xtmp[0], xtmp[1] - eps)) / (2 * eps);
-
-		double detf = F11 * F22 - F12 * F21;
-		if (detf < eps * eps) {
-			cout << "det(Jacobian) = 0" << endl;
+	for (size_t i = 0; i < N; ++i)
+		for (size_t j = 0; j < N; ++j) {
+			xNext[0] = -yR + hy * j;
+			xNext[1] = -xR + hx * i;
 			iter = 0;
-			return xcur;
+			do {
+				iter++;
+				if (iter > 30) {
+					iter = 30;
+					break;
+				}
+				xPrev = xNext;
+				//кринге, я согласен
+				if (analytical) {   //но по-другому не придумали
+					//test4
+					/*F11 = 2 * xPrev[0];
+					F12 = -2 * xPrev[1];
+					F21 = xPrev[1];
+					F22 = xPrev[0];*/
+
+					//test5
+					F11 = 2 * xPrev[0] + 1;
+					F12 = 2 * xPrev[1] + 1;
+					F21 = 2 * xPrev[0] + xPrev[1];
+					F22 = 2 * xPrev[1] + xPrev[0];
+				}
+				else {
+					F11 = (func1(xPrev[0] + h, xPrev[1]) - func1(xPrev[0] - h, xPrev[1])) / (2 * h);
+					F12 = (func1(xPrev[0], xPrev[1] + h) - func1(xPrev[0], xPrev[1] - h)) / (2 * h);
+					F21 = (func2(xPrev[0] + h, xPrev[1]) - func2(xPrev[0] - h, xPrev[1])) / (2 * h);
+					F22 = (func2(xPrev[0], xPrev[1] + h) - func2(xPrev[0], xPrev[1] - h)) / (2 * h);
+				}
+				//определитель матрицы Якоби
+				double detF = F11 * F22 - F12 * F21;
+				if (abs(detF) < 1e-12) {
+					iter = 0;
+					break;
+				}
+				xNext[0] = xPrev[0] - (F22 * func1(xPrev[0], xPrev[1]) - F12 * func2(xPrev[0], xPrev[1])) / detF;
+				xNext[1] = xPrev[1] - (-F21 * func1(xPrev[0], xPrev[1]) + F11 * func2(xPrev[0], xPrev[1])) / detF;
+			} while ((xPrev[0] - xNext[0])* (xPrev[0] - xNext[0]) + (xPrev[1] - xNext[1])*(xPrev[1] - xNext[1]) > eps);
+			square[i][j] = iter;
+			//if (iter < 3) \
+				cout << F11 << " " << F12 << " " << F21 << " " << F22 << "\n";
 		}
-		xprev = xcur;
-
-		double del1 = (F22 * func1(xtmp[0], xtmp[1]) - F12 * func2(xtmp[0], xtmp[1])) / detf;
-		double del2 = (-F21 * func1(xtmp[0], xtmp[1]) + F11 * func2(xtmp[0], xtmp[1])) / detf;
-		xcur[0] = xtmp[0] - del1;
-		xcur[1] = xtmp[1] - del2;
-
-
-		/*// Метод хорд, если вышли за допустимую область
-		if ((abs(xcur[0]) > L1) or (abs(xcur[1]) > L2)) {
-
-			cout << "Method Hord start: x_old =" << "(" << xcur[0] << ", " << xcur[1] << ")";
-			double a1 = xprev[0] - eps;
-			double b1 = xprev[0] + eps;
-
-			double a2 = xprev[1] - eps;
-			double b2 = xprev[1] + eps;
-
-			double f1a = g(a1, xprev[1], key1);
-			double f1b = g(xprev[0], b1, key1);
-
-			double f2a = g(a2, xprev[1], key2);
-			double f2b = g(xprev[0], b2, key2);
-
-			xcur[0] = (f1a * b1 - f1b * a1) / (f1a - f1b);
-			xcur[1] = (f2a * b2 - f2b * a2) / (f2a - f2b);
-			cout << " -> x_new = (" << xcur[0] << ", " << xcur[1] << ")" << endl;
-
-		} */
-
-
+	ofstream file("Grid.txt", ios_base::out);
+	file << xR << " " << yR << " " << N << endl;
+	for (size_t i = 0; i < N; ++i) {
+		for (size_t j = 0; j < N; ++j) {
+			file << square[i][j] << " ";
+		}
+		file << endl;
 	}
-	//cout << "END." << endl;
-	cout << "Количество итераций: " << iter << endl;
-	return xcur;
-};
+	file.close();
+	size_t pvSize = 39;
+	size_t cellSize = N / pvSize;
+	setlocale(0, "C");
+	for (long i = pvSize - 1; i >= 0; --i) {
+		for (size_t j = 0; j < pvSize; ++j) {
+			size_t i1 = cellSize * i;
+			size_t i2 = i1 + cellSize;
+			size_t j1 = cellSize * j;
+			size_t j2 = j1 + cellSize;
+			int value = -1;
+			for (size_t ii = i1; ii < i2; ++ii)
+				for (size_t jj = j1; jj < j2; ++jj)
+					if (square[ii][jj] == 0) {
+						value = 0;
+						break;
+					}
+					else if ((int)square[ii][jj] > value)
+						value = square[ii][jj];
+			if (value == 0)
+				cout << (unsigned char)219 << (unsigned char)219;
+			else if (value < 4)
+				cout << "  ";
+			else if (value < 7)
+				cout << (unsigned char)176 << (unsigned char)176;
+			else if (value < 10)
+				cout << (unsigned char)177 << (unsigned char)177;
+			else if (value < 20)
+				cout << (unsigned char)178 << (unsigned char)178;
+			else if (value < 40)
+				cout << (unsigned char)219 << (unsigned char)219;
+		}
+		cout << "\n";
+	};
+	return xNext;
+}
